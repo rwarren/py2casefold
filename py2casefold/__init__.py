ce4345f9da4c06c90d1f565ff2db7d136a471895
Cody Scott <cody.j.b.scott@gmail.com>
2015-10-27 20:17:33 -0400

    Cleaning up Python 3 compatibility names and passing the value into the ValueError

--- py2casefold/__init__.py
+++ py2casefold/__init__.py
@@ -1,22 +1,14 @@
 from __future__ import unicode_literals
-import sys
 import os
-
-PY3 = sys.version_info.major >= 3
-
-try:
-    _unichr = unichr
-except NameError:
-    _unichr = chr
-
-try:
-    _unicode_str = unicode
-except NameError:
-    _unicode_str = str
+import sys
 
 MAP_FILE = "CaseFolding.txt"
-unicode_version = ""
 _folding_map = {}
+PY3 = sys.version_info.major >= 3
+
+if PY3:
+    unichr = chr
+    unicode = str
 
 
 def set_unicode_version(first_line):
@@ -24,12 +16,11 @@ def set_unicode_version(first_line):
     unicode_version = first_line[first_line.find("-") + 1
                                  :first_line.find(".txt")]
 
-
 def _get_unichr(s):
     """Returns the unicode char matching the provided hex string index (s)."""
     try:
         # go for the fastest method first...
-        return _unichr(int(s, 16))
+        return unichr(int(s, 16))
     except ValueError: # wide char!
         # The case folding data contains some wide chars that unichr does not
         # support when python is not compiled with wide character support (very
@@ -40,7 +31,7 @@ def _get_unichr(s):
 def _read_unicode_data():
     global _folding_map
     map_path = os.path.join(os.path.dirname(__file__), MAP_FILE)
-    
+
     # Open the official CaseFolding.txt file to read the folding map...
     #  - Codecs.open would be nice, but I'm trying to limit imports here
     #  - Although I can't find a reference for what encoding the Unicode
@@ -55,7 +46,7 @@ def _read_unicode_data():
     fp.close()
     # The Unicode version is on the first line of the file.
     set_unicode_version(lines[0])
-    
+
     for line in lines:
         if not PY3:
             line = line.decode("utf-8")
@@ -75,10 +66,10 @@ def casefold(u):
 
     ValueError is raised if u is not a unicode instance. casefold is a unicode
     concept.
-    
+
     """
-    if not isinstance(u, _unicode_str):
-        raise ValueError("u must be unicode")
+    if not isinstance(u, unicode):
+        raise ValueError("%s must be unicode" % u)
     return "".join(_folding_map.get(c, c) for c in u)
 
 # read/stash the folding map on import...
